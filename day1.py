aa7ed237ce843fd2011f49afb934ecdcc5cf42a0
Pylyp Maliuta <pylyp.maliuta@stuzo.com>
2022-07-10 21:36:36 +0300

    arrays

---
+++ day1.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+a1 = np.array([1, 2, 3])
+
+a2 = np.array([[1, 2, 3],
+               [4, 5, 6]])
+a3 = np.array([[[ 1, 2, 3],
+                [4, 5, 6]],
+               [[7, 8, 9],
+                [10, 11, 12]],
+               [[13, 14, 15],
+                [16, 17, 18]]])
+
+df = pd.DataFrame(a2)
