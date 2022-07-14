4f1a24de966bee30050f90b9bd1e2b8bad9efcab
Pylyp Maliuta <pylyp.maliuta@stuzo.com>
2022-07-14 11:12:35 +0300

    day5: exploring columns

--- day4_pandas.py
+++ day4_pandas.py
@@ -10,3 +10,19 @@ car_sales_csv = pd.read_csv("car-sales.txt")
 heart_disease = pd.read_csv("https://raw.githubusercontent.com/mrdbourke/zero-to-mastery-ml/master/data/heart-disease.csv")
 
 car_sales_csv.to_csv("exported_car_sales.csv", index = False)
+
+print(car_sales_csv.dtypes)
+print(car_sales_csv.index)
+
+print(car_sales_csv.describe())
+
+print(car_sales_csv.info())
+
+print(car_sales_csv.mean())
+
+print(car_sales_csv.sum())
+
+print(car_sales_csv["Doors"].sum())
+
+print(len(car_sales_csv))
+
